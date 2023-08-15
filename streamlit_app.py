a51a384e3e3dc92dd495ef2a28217312eaca7d02
NURFAZILLA RAHIM <88186504+nurfazilla@users.noreply.github.com>
2023-08-15 09:46:44 +0800

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,46 +3,46 @@ import streamlit as st
 #### Define questions and point system
 questions = {
     "1. What is your main source of income?": {
-        "a.Uncertain source of income": 1,
-        "b.Have a stable source of income": 2,
-        "c.Have a stable and comfortable source of income": 3,
-        "d.Have various sources of stable income": 4,
-        "e.Having various sources of strong active and passive income": 5
+        "Uncertain source of income": 1,
+        "Have a stable source of income": 2,
+        "Have a stable and comfortable source of income": 3,
+        "Have various sources of stable income": 4,
+        "Having various sources of strong active and passive income": 5
     },
     "2. How are you budgeting your monthly expenses according to your income?": {
-        "a.The amount of monthly expenses exceeds income": 1,
-        "b.Budget monthly expenses according to income": 2,
-        "c.Have a monthly surplus after expenses": 3,
-        "d.Monthly expenses are much lower compared to income": 4,
-        "e.One or two sources of income can cover monthly expenses": 5
+        "The amount of monthly expenses exceeds income": 1,
+        "Budget monthly expenses according to income": 2,
+        "Have a monthly surplus after expenses": 3,
+        "Monthly expenses are much lower compared to income": 4,
+        "One or two sources of income can cover monthly expenses": 5
     },
     "3. How much do you currently have in savings?": {
-        "a.Not able to save": 1,
-        "b.Able to save but do not have savings": 2,
-        "c.Have savings equivalent to 1-6 months of income": 3,
-        "d.Have savings equal to 6-12 months of income": 4,
-        "e.Have a savings amount that exceeds the amount of 12 months of income": 5
+        "Not able to save": 1,
+        "Able to save but do not have savings": 2,
+        "Have savings equivalent to 1-6 months of income": 3,
+        "Have savings equal to 6-12 months of income": 4,
+        "Have a savings amount that exceeds the amount of 12 months of income": 5
     },
     "4. Do you own any assets for personal use?": {
-        "a.No assets": 1,
-        "b.Own assets for personal use": 2,
-        "c.Own assets for personal use and some assets for investment purposes": 3,
-        "d.Owning assets for own use and family members as well as various assets for investment purposes": 4,
-        "e.Owning assets for personal use and family members as well as various assets that provide consistent returns": 5
+        "No assets": 1,
+        "Own assets for personal use": 2,
+        "Own assets for personal use and some assets for investment purposes": 3,
+        "Owning assets for own use and family members as well as various assets for investment purposes": 4,
+        "Owning assets for personal use and family members as well as various assets that provide consistent returns": 5
     },
     "5.Are you able to pay off your debts with your current income?": {
-        "a.Earned income is not able to pay debt": 1,
-        "b.Earned income is able to pay off debt": 2,
-        "c.Earned income is able to pay off debt and have excess funds after paying off debt": 3,
-        "d.Earned income able to pay off debt and able to generate additional income": 4,
-        "e.Free from debt": 5
+        "Earned income is not able to pay debt": 1,
+        "Earned income is able to pay off debt": 2,
+        "Earned income is able to pay off debt and have excess funds after paying off debt": 3,
+        "Earned income able to pay off debt and able to generate additional income": 4,
+        "Free from debt": 5
     },
     "6.Do you have any insurance protection?": {
-        "a.No insurance protection": 1,
-        "b.Have minimum insurance protection for yourself or insurance coverage from the employer only": 2,
-        "c.Have insurance protection equivalent to approximately the annual income for yourself and family members": 3,
-        "d.Have insurance protection equivalent to twice or triple your annual income for yourself and family members": 4,
-        "e.Have insurance protection exceeding three times the annual income for yourself and family members": 5
+        "No insurance protection": 1,
+        "Have minimum insurance protection for yourself or insurance coverage from the employer only": 2,
+        "Have insurance protection equivalent to approximately the annual income for yourself and family members": 3,
+        "Have insurance protection equivalent to twice or triple your annual income for yourself and family members": 4,
+        "Have insurance protection exceeding three times the annual income for yourself and family members": 5
     },  
 }
 
@@ -50,18 +50,23 @@ questions = {
 result_categories = {
     "Financial Instability": {
         "range": (6, 12),
+        "image": "https://drive.google.com/file/d/1KoqVxhHt_KT3UjUGSHa1189D-TTwTrjv/view?usp=sharing"
     },
     "Financial Stability": {
         "range": (13, 18),
+        "image": "https://drive.google.com/file/d/1D35SRj3doyi4C5iT-53qYPn2u9H4WFEd/view?usp=sharing",
     },
     "Financial Security": {
         "range": (19, 24),
+        "image": "https://drive.google.com/file/d/1NIJIGB2a3_nODstxYNHREjLwnRw8lHjW/view?usp=sharing"
     },
     "Financial Freedom": {
         "range": (25, 30),
+        "image": "https://drive.google.com/file/d/1XZvyTTE9_cetJFXfgVTTOwvtNqzLy76s/view?usp=sharing"
     },
     "Financial Wealth": {
         "range": (31, 36),
+         "image": "https://drive.google.com/file/d/12xyFs81ORqFAq3ZQKOq70SaNhNlGvLHa/view?usp=sharing"
     },
 }
 
@@ -94,6 +99,9 @@ def display_result(result_category):
     st.subheader("Result:")
     if result_category:
         st.write(f"Based on your responses, you are in the '{result_category}' category.")
+        result_image = result_categories[result_category].get("image")
+        if result_image:
+            st.image(result_image, caption="Category Image", use_column_width=True)
 
 if __name__ == "__main__":
     main()
